2af7fa95b8f41a66e2341da645b82614e73eb054
noz <nowfeel.safi@gmail.com>
2025-12-06 11:55:12 +0100

    feat: register notary by contract owner, create will by notary and setup by testator, add Owner Openzeppelin wip need to fix unit test

--- backend/contracts/Wallegacy.sol
+++ backend/contracts/Wallegacy.sol
@@ -22,18 +22,18 @@ import "./WallegacySBT.sol";
     6. Functions 
 */
 
-contract Wallegacy {
+contract Wallegacy is Ownable {
     enum WillStatus {
         DRAFT, // DRAFT is the gasPayed property is set to false, the Will is mandatory DRAFT
         SAVED, // SAVED is used when all necessaries elements of the Will are correctly set
         DONE, // DONE is used when funds has been sent to heirs
-        CANCELLED
+        CANCELLED // CANCELLES is used only when a Testator call CancelWill() function
     }
 
     struct Will {
         address testator;
         WillStatus status;
-        bool gasPayed;
+        bool gasPayed; // DEPRECATED - TO BE REMOVED
         bool exists; // for getter function
         Heir[] heirs;
         address notary;
@@ -54,7 +54,7 @@ contract Wallegacy {
 
     WallegacySBT public sbtContract;
 
-    event WillCreated(address indexed testator);
+    event WillSetUp(address indexed testator);
     event TestatorValueLocked(address indexed testator, uint256 amount);
     event LegacySentToHeir(address indexed heirAddress);
     event LegacySent(address indexed testatorAddress);
@@ -62,6 +62,14 @@ contract Wallegacy {
     event SBTContractSet(address indexed sbtAddess);
     event NotaryCancelWill(address indexed notaryAddress);
     event NotaryNoMoreWill(address indexed notaryAddress);
+    event NotaryNewWill(
+        address indexed notaryAddress,
+        address indexed testatorAddress
+    );
+    event NotaryRegistered(address indexed notaryAddress);
+    event TestatorRegistered(address indexed testatorAddress);
+    event NotaryAddWill(address indexed notaryAddress);
+    event NotaryRemovedWill(address indexed notaryAddress);
 
     error Wallegacy__WillNotFound(address testatorAddress);
     error Wallegacy__NoHeirs();
@@ -76,14 +84,17 @@ contract Wallegacy {
         uint256 heirAmount
     );
     error Wallegacy__WillDone();
-    error Wallegacy__NoTestator();
+    error Wallegacy__NoTestator(address sender);
     error Wallegacy__TestatorAlreadyHasWill(address testatorAddress);
     error WallegacySBT__NoAddress();
     error WallegacySBT__NotSet();
     error Wallegacy__Unauthorized();
     error Wallegacy__NewWillMissingNotary();
+    error Wallegacy__WillAlreadySet(address testatorAddress);
+    error Wallegacy__TestatorWithoutWill(address sender);
+    error Wallegacy__NotaryNegativeNumberOfWill(address notaryAddress);
 
-    constructor() {}
+    constructor() Ownable(msg.sender) {}
 
     modifier onlyNotary() {
         if (!s_notaries[msg.sender]) {
@@ -94,7 +105,7 @@ contract Wallegacy {
 
     modifier onlyTestator() {
         if (!s_testators[msg.sender]) {
-            revert Wallegacy__NoTestator();
+            revert Wallegacy__NoTestator(msg.sender);
         }
         _;
     }
@@ -124,6 +135,10 @@ contract Wallegacy {
         return s_notaries[notaryAddress];
     }
 
+    function isTestator() external view returns (bool) {
+        return s_testators[msg.sender];
+    }
+
     function setSBTContract(address _sbtContract) external {
         if (_sbtContract == address(0)) {
             revert WallegacySBT__NoAddress();
@@ -133,8 +148,35 @@ contract Wallegacy {
         emit SBTContractSet(_sbtContract);
     }
 
+    function registerNotary(address notaryAddress) public onlyOwner {
+        s_notaries[notaryAddress] = true;
+
+        emit NotaryRegistered(notaryAddress);
+    }
+
+    function registerTestator(address testatorAddress) private {
+        s_testators[testatorAddress] = true;
+
+        emit TestatorRegistered(testatorAddress);
+    }
+
+    function addWillToNotary() private onlyNotary {
+        s_notaryToNumberOfWill[msg.sender]++;
+
+        emit NotaryAddWill(msg.sender);
+    }
+
+    function removeWillToNotary(address notaryAddress) private {
+        s_notaryToNumberOfWill[notaryAddress]--;
+        if (s_notaryToNumberOfWill[notaryAddress] < 0) {
+            revert Wallegacy__NotaryNegativeNumberOfWill(notaryAddress);
+        }
+
+        emit NotaryRemovedWill(notaryAddress);
+    }
+
     /// todo: add a check if Will exists here to allow people to fund the contract ?
-    function lockTestatorFunds() public payable {
+    function lockTestatorFunds() public payable onlyTestator {
         // revert with custom errors is more gas efficient than require
         if (msg.value <= 0) {
             revert Wallegacy__NotEnoughAmount();
@@ -145,20 +187,34 @@ contract Wallegacy {
         emit TestatorValueLocked(msg.sender, msg.value);
     }
 
-    function isTestator() external view returns (bool) {
-        return s_testators[msg.sender];
-    }
-
-    /// @dev the status is always set to DRAFT on creation
-    /// TODO: add a check if the user has set multiple heirs with the same address ?
-    function createWill(
-        Heir[] memory heirsParams,
-        address notaryAddress
-    ) public payable onlyWithSBTContractSet {
-        if (s_testators[msg.sender]) {
-            revert Wallegacy__TestatorAlreadyHasWill(msg.sender);
+    /// @dev this function should only be called by a notary to setup the Will of a Testator
+    /// @dev after this processs, it should be the testator who will update the Will setting up his heirs etc
+    function newWill(address testatorAddress) public onlyNotary {
+        if (s_testatorToWill[testatorAddress].exists) {
+            revert Wallegacy__WillAlreadySet(testatorAddress);
         }
 
+        Heir[] memory newHeirs;
+
+        s_testatorToWill[testatorAddress] = Will({
+            testator: testatorAddress,
+            status: WillStatus.DRAFT,
+            gasPayed: false,
+            exists: true,
+            heirs: newHeirs,
+            notary: msg.sender
+        });
+
+        registerTestator(testatorAddress);
+        addWillToNotary();
+
+        emit NotaryNewWill(msg.sender, testatorAddress);
+    }
+
+    /// TODO: add a check if the user has set multiple heirs with the same address ?
+    function setUpWill(
+        Heir[] memory heirsParams
+    ) public payable onlyWithSBTContractSet onlyTestator {
         if (heirsParams.length == 0) {
             revert Wallegacy__NoHeirs();
         }
@@ -167,8 +223,9 @@ contract Wallegacy {
             revert Wallegacy__NotEnoughAmount();
         }
 
-        if (notaryAddress == address(0)) {
-            revert Wallegacy__NewWillMissingNotary();
+        Will memory testatorWill = s_testatorToWill[msg.sender];
+        if (!testatorWill.exists) {
+            revert Wallegacy__TestatorWithoutWill(msg.sender);
         }
 
         uint8 totalPercent = 0;
@@ -186,28 +243,15 @@ contract Wallegacy {
             revert Wallegacy__NewWillNotGoodPercent(totalPercent);
         }
 
-        // lock the value of the Testator
+        // lock the funds of the Testator
         lockTestatorFunds();
-
-        s_testatorToWill[msg.sender] = Will({
-            testator: msg.sender,
-            status: WillStatus.SAVED,
-            gasPayed: false,
-            exists: true,
-            heirs: heirsParams,
-            notary: notaryAddress
-        });
 
         sbtContract.mint(msg.sender);
 
-        s_testators[msg.sender] = true;
-        s_notaryToNumberOfWill[notaryAddress]++;
+        testatorWill.heirs = heirsParams;
+        testatorWill.status = WillStatus.SAVED;
 
-        if (!isNotary(notaryAddress)) {
-            s_notaries[notaryAddress] = true;
-        }
-
-        emit WillCreated(msg.sender);
+        emit WillSetUp(msg.sender);
     }
 
     function cancelWill() public onlyTestator onlyWithSBTContractSet {
@@ -216,27 +260,10 @@ contract Wallegacy {
         s_testatorToWill[msg.sender].status = WillStatus.CANCELLED;
         s_testators[msg.sender] = false;
 
-        address notaryAddress = s_testatorToWill[msg.sender].notary;
-        s_notaryToNumberOfWill[notaryAddress]--;
-        if (s_notaryToNumberOfWill[notaryAddress] == 0) {
-            s_notaries[notaryAddress] = false;
-        }
-
+        removeWillToNotary(s_testatorToWill[msg.sender].notary);
         sbtContract.burn(msg.sender);
 
         emit WillCancelled(msg.sender);
-    }
-
-    // is it ok to set a revert in private function ?
-    function cancelNotary(address notaryAddress) private {
-        if (isNotary(notaryAddress)) {
-            s_notaryToNumberOfWill[notaryAddress]--;
-            emit NotaryCancelWill(notaryAddress);
-            if (s_notaryToNumberOfWill[notaryAddress] == 0) {
-                s_notaries[notaryAddress] = false;
-                emit NotaryNoMoreWill(notaryAddress);
-            }
-        }
     }
 
     function triggerLegacyProcess(address testatorAddress) public onlyNotary {
@@ -246,7 +273,7 @@ contract Wallegacy {
     /// todo manage correctly the rest
     function sendLegacyToHeirs(address testatorAddress) private {
         if (!s_testators[testatorAddress]) {
-            revert Wallegacy__NoTestator();
+            revert Wallegacy__NoTestator(testatorAddress);
         }
 
         Will storage testatorWill = s_testatorToWill[testatorAddress];
@@ -254,9 +281,9 @@ contract Wallegacy {
 
         // we first change the state of the contract before sending ETH to avoid reentrancy
         // pattern Check Effect Interaction (CEI)
-        // we also can use OpenZeppelin library
+        // we also can use OpenZeppelin library to setup a Guard (double check)
         testatorWill.status = WillStatus.DONE;
-        cancelNotary(msg.sender);
+        removeWillToNotary(testatorWill.notary);
 
         // we compute the amount to send and we send it
         for (uint256 i = 0; i < testatorWill.heirs.length; i++) {
